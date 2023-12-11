e8aeb2bc242cf391b82eb67705c4e9c0c6786dc8
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2023-12-11 21:17:11 +0500

    fixed bugs

--- main.js
+++ main.js
@@ -30,3 +30,61 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('loadingGif').style.display = 'none';
     });
 });
+
+
+// carousel
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Function to change slides manually by button clicks
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Function to change slides by dot clicks
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+// Main function to display slides
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    // Reset if index goes out of range
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    // Hide all slides and remove active dot class
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    // Show current slide and set corresponding dot as active
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Automatic slideshow function
+var slideInterval = setInterval(function() {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 5000);
+
+// Pauses automatic slideshow on manual interaction
+var slideshowContainer = document.getElementsByClassName("slideshow-container")[0];
+slideshowContainer.addEventListener('mouseenter', function() {
+    clearInterval(slideInterval);
+});
+
+slideshowContainer.addEventListener('mouseleave', function() {
+    slideInterval = setInterval(function() {
+        slideIndex++;
+        showSlides(slideIndex);
+    }, 5000);
+});
