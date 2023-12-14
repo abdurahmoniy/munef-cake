859d41849a086d61ec85b03efb6b2cd44c3f0bba
abdurahmoniy <azizbekabdurahmoniy@gmail.com>
2023-12-14 22:31:09 +0500

    fixed bugs

--- main.js
+++ main.js
@@ -86,3 +86,58 @@ slideshowContainer.addEventListener("mouseleave", function () {
   }, 3000);
 });
 
+//language switcher
+
+document.getElementById('selectedLang').addEventListener('click', function() {
+  var languageOptions = document.getElementById('languageOptions');
+  if (languageOptions.style.display === 'none') {
+      languageOptions.style.display = 'block';
+  } else {
+      languageOptions.style.display = 'none';
+  }
+});
+
+document.getElementById('uzbLang').addEventListener('click', function() {
+  changeLanguage('uzbflag.png', 'Uzb');
+});
+
+document.getElementById('rusLang').addEventListener('click', function() {
+  changeLanguage('rusflag.png', 'Rus');
+});
+
+document.getElementById('engLang').addEventListener('click', function() {
+  changeLanguage('engflag.png', 'Eng');
+});
+
+function changeLanguage(flagImage, languageText) {
+  document.getElementById('selectedLang').innerHTML = '<img src="./img/' + flagImage + '" alt="">' + languageText;
+  document.getElementById('languageOptions').style.display = 'none';
+}
+
+//language switcher2
+
+document.getElementById('selectedLang2').addEventListener('click', function() {
+  var languageOptions2 = document.getElementById('languageOptions2');
+  if (languageOptions2.style.display === 'none') {
+      languageOptions2.style.display = 'block';
+  } else {
+      languageOptions2.style.display = 'none';
+  }
+});
+
+document.getElementById('uzbLang2').addEventListener('click', function() {
+  changeLanguage2('uzbflag.png', 'Uzb');
+});
+
+document.getElementById('rusLang2').addEventListener('click', function() {
+  changeLanguage2('rusflag.png', 'Rus');
+});
+
+document.getElementById('engLang2').addEventListener('click', function() {
+  changeLanguage2('engflag.png', 'Eng');
+});
+
+function changeLanguage2(flagImage2, languageText2) {
+  document.getElementById('selectedLang2').innerHTML = '<img src="./img/' + flagImage2 + '" alt="">' + languageText2;
+  document.getElementById('languageOptions2').style.display = 'none';
+}
